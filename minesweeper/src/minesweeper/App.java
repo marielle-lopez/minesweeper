b85f51119509ee94ed21dcf03c03601af40fc9a2
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-02-12 20:03:25 +1100

    enabled user input through console

--- minesweeper/src/minesweeper/App.java
+++ minesweeper/src/minesweeper/App.java
@@ -1,10 +1,34 @@
 package minesweeper;
 
+import java.util.Scanner;
+
 public class App {
 
 	public static void main(String[] args) {
 		Grid grid = new Grid();
-//		grid.printGrid();
+		grid.printGrid();
+		boolean isGameOver = false;
+		String input = "";		
+		Scanner s = new Scanner(System.in);
+		
+		while (isGameOver == false && !input.equalsIgnoreCase("q")) {
+			String coordinate = "";
+
+			System.out.println("Select a row (1 to 10): ");
+			input = s.nextLine();
+			coordinate += String.format("r%d", Integer.parseInt(input) - 1);
+			
+			System.out.println("Select a column (1 to 10): ");
+			input = s.nextLine();
+			coordinate += String.format("c%d", Integer.parseInt(input) - 1);
+			
+			grid.openCell(coordinate);
+			
+			System.out.println(coordinate);
+		}
+		
+		System.out.println("End of game.");
+		s.close();
 	}
 	
 }
