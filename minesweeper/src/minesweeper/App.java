b8abe9375342a93f44d7acea800fcb2e660e55c9
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-02-15 22:30:01 +1100

    terminate game when user selects all non-bomb cells

--- minesweeper/src/minesweeper/App.java
+++ minesweeper/src/minesweeper/App.java
@@ -6,14 +6,20 @@ public class App {
 
 	public static void main(String[] args) {
 		Grid grid = new Grid();
+		int maxTurns = (int) Math.pow(grid.getLength(), 2) - grid.getLength();
+		int turnsCount = 0;
 		grid.printGrid(false);
-		System.out.println(grid.getBombsCoordinates());
+		
+//		for (Map.Entry<String, Cell> set: grid.getBombsCoordinates().entrySet()) {
+//			System.out.println(set);
+//		}
+		
 		boolean isGameOver = false;
 		
 		String input = "";		
 		Scanner s = new Scanner(System.in);
 		
-		while (isGameOver == false && !input.equalsIgnoreCase("q")) {
+		while (isGameOver == false && !input.equalsIgnoreCase("q") && turnsCount < maxTurns) {
 			String coordinate = "";
 
 			System.out.println("Select a row (1 to 10): ");
@@ -26,6 +32,12 @@ public class App {
 			
 			isGameOver = grid.openCell(coordinate);
 			grid.printGrid(isGameOver);
+			
+			turnsCount++;
+			
+			if (turnsCount == maxTurns) {
+				System.out.println("You've won!");
+			}
 		}
 		
 		System.out.println("End of game.");
