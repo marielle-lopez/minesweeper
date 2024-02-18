a2207dba7e6bb8dd81323a1bd36fe9c7b857270c
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-02-18 15:48:31 +1100

    removed code from classes to begin TDD

--- minesweeper/src/minesweeper/App.java
+++ minesweeper/src/minesweeper/App.java
@@ -1,47 +1,9 @@
 package minesweeper;
 
-import java.util.Scanner;
-
 public class App {
 
 	public static void main(String[] args) {
-		Grid grid = new Grid();
-		int maxTurns = (int) Math.pow(grid.getLength(), 2) - grid.getLength();
-		int turnsCount = 0;
-		grid.printGrid(false);
-		
-//		for (Map.Entry<String, Cell> set: grid.getBombsCoordinates().entrySet()) {
-//			System.out.println(set);
-//		}
-		
-		boolean isGameOver = false;
-		
-		String input = "";		
-		Scanner s = new Scanner(System.in);
-		
-		while (isGameOver == false && !input.equalsIgnoreCase("q") && turnsCount < maxTurns) {
-			String coordinate = "";
 
-			System.out.println("Select a row (1 to 10): ");
-			input = s.nextLine();
-			coordinate += String.format("r%d", Integer.parseInt(input) - 1);
-			
-			System.out.println("Select a column (1 to 10): ");
-			input = s.nextLine();
-			coordinate += String.format("c%d", Integer.parseInt(input) - 1);
-			
-			isGameOver = grid.openCell(coordinate);
-			grid.printGrid(isGameOver);
-			
-			turnsCount++;
-			
-			if (turnsCount == maxTurns) {
-				System.out.println("You've won!");
-			}
-		}
-		
-		System.out.println("End of game.");
-		s.close();
 	}
 	
 }
--- minesweeper/src/minesweeper/Cell.java
+++ minesweeper/src/minesweeper/Cell.java
@@ -1,48 +1,5 @@
 package minesweeper;
 
-import java.util.Arrays;
-
 public class Cell {
-	private int[] coordinates = {-1, -1};
-	private boolean hasBomb;
-	private boolean hasBeenOpened = false;
-	private int surroundingBombsCount;
-	
-	public Cell(int row, int column, boolean hasBomb) {
-		this.coordinates[0] = row;
-		this.coordinates[1] = column;
-		this.hasBomb = hasBomb;
-	};
-	
-	public void setHasBomb(boolean hasBomb) {
-		this.hasBomb = hasBomb;
-	}
-	
-	public boolean getHasBomb() {
-		return this.hasBomb;
-	}
-	
-	public void setSurroundingBombsCount(int surroundingBombsCount) {
-		this.surroundingBombsCount = surroundingBombsCount;
-	};
-	
-	public int getSurroundingBombsCount() {
-		return this.surroundingBombsCount;
-	}
-	
-	public void setHasBeenOpened(boolean hasBeenOpened) {
-		this.hasBeenOpened = hasBeenOpened;
-	};
-	
-	public boolean getHasBeenOpened() {
-		return this.hasBeenOpened;
-	}
 
-	@Override
-	public String toString() {
-		return "Cell [coordinates=" + Arrays.toString(coordinates) + ", hasBomb=" + hasBomb + ", hasBeenOpened="
-				+ hasBeenOpened + ", surroundingBombsCount=" + surroundingBombsCount + "]";
-	};
-	
-	
 }
