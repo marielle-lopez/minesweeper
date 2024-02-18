8f64c49345eb97299907456795eca2a373cacab0
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-02-18 19:16:26 +1100

    created method to generate grid row labels

--- minesweeper/src/minesweeper/Grid.java
+++ minesweeper/src/minesweeper/Grid.java
@@ -42,14 +42,38 @@ public class Grid {
 		return middleLine;
 	}
 	
+	public String generateColumnGridLabels() {
+		String columnLabels = "  ";
+		for (int i = 1; i <= this.length; i++) {
+			if (i < 10) {				
+				columnLabels += String.format("   %s", i);
+			} else {
+				columnLabels += String.format("  %s", i);
+			}
+		}
+		return columnLabels;
+	}
+	
+	public String generateRowGridLabel(int rowNumber) {
+		String rowLabel;
+		if (rowNumber < 10) {
+			rowLabel = String.format(" %d ", rowNumber);
+		} else {
+			rowLabel = String.format("%d ", rowNumber);
+		}
+		return rowLabel;
+	}
+	
 	public void printGrid() {
+		System.out.println(this.generateColumnGridLabels());
 		System.out.println(String.format("   %s", this.generateTopGridLine()));
 		for (int i = 1; i < this.length; i++) {
-			System.out.print(String.format(" %d ", i));
+			System.out.print(this.generateRowGridLabel(i));
 			System.out.println(this.generateCellGridLines());
 			System.out.println(String.format("   %s", this.generateMiddleGridLine()));
 		}
-		System.out.println(String.format("%d %s", this.length, this.generateCellGridLines()));
+		System.out.print(this.generateRowGridLabel(this.length));
+		System.out.println(this.generateCellGridLines());
 		System.out.println(String.format("   %s", this.generateBottomGridLine()));
 	}
 }
