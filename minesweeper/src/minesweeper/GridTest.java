a303f419052c6d59873ef8f2670b8417fc5581b1
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-02-18 18:38:50 +1100

    updated tests to assert updated grid design

--- minesweeper/src/minesweeper/GridTest.java
+++ minesweeper/src/minesweeper/GridTest.java
@@ -10,48 +10,48 @@ class GridTest {
 	void generateTopGridLine_LengthIs10_ReturnsTopLineString() {
 		Grid grid = new Grid(10);
 		String generatedTopLine = grid.generateTopGridLine();
-		assertEquals("┌─┬─┬─┬─┬─┬─┬─┬─┬─┬─┐", generatedTopLine);
+		assertEquals("┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐", generatedTopLine);
 	}
 	
 	@Test
 	void generateTopGridLine_LengthIs5_ReturnsTopLineString() {
 		Grid grid = new Grid(5);
 		String generatedTopLine = grid.generateTopGridLine();
-		assertEquals("┌─┬─┬─┬─┬─┐", generatedTopLine);
+		assertEquals("┌───┬───┬───┬───┬───┐", generatedTopLine);
 	}
 
 	@Test
 	void generateBottomGridLine_LengthIs10_ReturnsBottomLineString() {
 		Grid grid = new Grid(10);
 		String generatedBottomLine = grid.generateBottomGridLine();
-		assertEquals("└─┴─┴─┴─┴─┴─┴─┴─┴─┴─┘", generatedBottomLine);
+		assertEquals("└───┴───┴───┴───┴───┴───┴───┴───┴───┴───┘", generatedBottomLine);
 	}
 	
 	@Test
 	void generateBottomGridLine_LengthIs4_ReturnsBottomLineString() {
 		Grid grid = new Grid(4);
 		String generatedBottomLine = grid.generateBottomGridLine();
-		assertEquals("└─┴─┴─┴─┘", generatedBottomLine);
+		assertEquals("└───┴───┴───┴───┘", generatedBottomLine);
 	}
 	
 	@Test
 	void generateCellGridLines_LengthIs5_ReturnsCellGridLinesString() {
 		Grid grid = new Grid(5);
 		String generatedCellLines = grid.generateCellGridLines();
-		assertEquals("│ │ │ │ │ │", generatedCellLines);
+		assertEquals("│   │   │   │   │   │", generatedCellLines);
 	}
 	
 	@Test
 	void generateCellGridLines_LengthIs10_ReturnsCellGridLinesString() {
 		Grid grid = new Grid(10);
 		String generatedCellLines = grid.generateCellGridLines();
-		assertEquals("│ │ │ │ │ │ │ │ │ │ │", generatedCellLines);
+		assertEquals("│   │   │   │   │   │   │   │   │   │   │", generatedCellLines);
 	}
 	
 	@Test 
 	void generateMiddleGridLine_LengthIs10_ReturnsMiddleGridLineString() {
 		Grid grid = new Grid(10);
 		String generatedMiddleLine = grid.generateMiddleGridLine();
-		assertEquals("├─┼─┼─┼─┼─┼─┼─┼─┼─┼─┤", generatedMiddleLine);
+		assertEquals("├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤", generatedMiddleLine);
 	}
 }
