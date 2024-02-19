5b84684d166b5ce58ac9370077205289e8180896
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-02-20 09:31:20 +1100

    enabled user input for cell selection

--- minesweeper/src/minesweeper/App.java
+++ minesweeper/src/minesweeper/App.java
@@ -1,10 +1,32 @@
 package minesweeper;
 
+import java.util.Scanner;
+
 public class App {
 
 	public static void main(String[] args) {
-		Grid grid = new Grid(11);
-		grid.printGrid();
+		Grid grid = new Grid(3, 3);
+		int maxTurns = (int) Math.pow(grid.getLength(), 2) - grid.getBombCount();
+		int turnCount = 0;
+		
+		Scanner s = new Scanner(System.in);
+		String userInput = "";
+		
+		while (userInput.toLowerCase() != "q" && turnCount < maxTurns) {
+			System.out.println("Select a row: ");
+			userInput = s.nextLine();
+			int row = Integer.valueOf(userInput);
+			System.out.println("Select a column: ");
+			userInput = s.nextLine();
+			int column = Integer.valueOf(userInput);
+			grid.openCell(row, column);
+			turnCount++;
+			if (turnCount == maxTurns) {
+				System.out.println("You have won!");
+			}
+		}
+		
+		s.close();
 	}
 	
 }
